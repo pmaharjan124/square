5f9d5cbbaf13194de5c14d13cac3ef697d3e937f
pmaharjan124 <maharjanpuja@gmail.com>
2020-04-15 23:46:20 -0400

    third commit

--- src/Square.java
+++ src/Square.java
@@ -9,7 +9,7 @@ public class Square {
             System.out.print("Please enter a number: ");
             number = sc.nextInt();
             sc.nextLine();
-                System.out.println("Number\t\t\tSquare\n\t\t");
+                System.out.println("Number\t\t\tSquare\n....\t\t....");
                 for (int i =1; i <= number; i++) {
                     square = i * i;
                     System.out.printf("%d\t\t\t%d\n", i, square);
